be6b014aa11a605a74427135a3b53c89baf510b1
Kamil <kubala.kamil11@gmail.com>
2021-01-26 12:37:05 +0100

    Docker, routing, Leaflet init

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('map', 'DefaultController');
+
+
+
+Router::run($path);
