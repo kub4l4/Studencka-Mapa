8c833b155af3fa8e7a183c8e68d6b759f51e691d
Kamil <kubala.kamil11@gmail.com>
2021-01-29 01:30:59 +0100

    Created geojson files, added to map, created layercontrol

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -1,76 +1,106 @@
 var map = L.map('map').setView([50.06, 19.94], 14);
+
 L.tileLayer('https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=uBf51zOK4P3YedN0PRlg', {
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a>' +
+        ' <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
 }).addTo(map);
+
 
 L.Control.Watermark = L.Control.extend({
     onAdd: function (map) {
-        var img = L.DomUtil.create('img');
+        const img = L.DomUtil.create('img');
         img.src = '/public/img/logo.svg';
-        img.style.width = '100px';
+        img.style.width = '20em';
         return img;
     },
     onRemove: function (map) {
     },
 });
+L.control.watermark = function(opts) {
+    return new L.Control.Watermark(opts);
+}
+L.control.watermark({ position: 'bottomright' }).addTo(map);
 
 
-var LeaffletIcon = L.Icon.extend({
-    options: {
-        iconSize: [38, 95],
-        iconAnchor: [22, 94],
-        popupAnchor: [12, -90],
-        //shadowUrl: 'https://leafletjs.com/examples/custom-icons/leaf-shadow.png',
-        shadowSize: [50, 64],
-        shadowAnchor: [4, 62]
-    }
-})
+var bydynki = L.geoJSON().addTo(map);
+$.getJSON("/public/geojson/budynki.geojson",function(data){
+    bydynki.addData(data);
+});
 
-//var greenIcon = new LeaffletIcon({iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-green.png'}),
-//    redIcon = new LeaffletIcon({iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-red.png'}),
-//    orageIcon = new LeaffletIcon({iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-orange.png'})
+var jednostkiPozawydzialowe = L.geoJSON().addTo(map);
+$.getJSON("/public/geojson/Jednostki_pozawydzialowe.geojson",function(data){
+    jednostkiPozawydzialowe.addData(data);
+});
 
+var WA = L.geoJSON().addTo(map);
+$.getJSON("/public/geojson/WA.geojson",function(data){
+    WA.addData(data);
+});
 
-var leafletIcon1 = L.icon({
-   // iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-green.png',
-    iconSize: [38, 95],
-    iconAnchor: [22, 94],
-    popupAnchor: [12, -90],
-  //  shadowUrl: 'https://leafletjs.com/examples/custom-icons/leaf-shadow.png',
-    shadowSize: [50, 64],
-    shadowAnchor: [4, 62]
+var WIEIK = L.geoJSON().addTo(map);
+$.getJSON("/public/geojson/WIEIK.geojson",function(data){
+    WIEIK.addData(data);
+});
+
+var WIIT = L.geoJSON().addTo(map);
+$.getJSON("/public/geojson/WIIT.geojson",function(data){
+    WIIT.addData(data);
+});
+
+var WIITCH = L.geoJSON().addTo(map);
+$.getJSON("/public/geojson/WIITCH.geojson",function(data){
+    WIITCH.addData(data);
+});
+
+var WIL = L.geoJSON().addTo(map);
+$.getJSON("/public/geojson/WIL.geojson",function(data){
+    WIL.addData(data);
+});
+
+var WIMIF = L.geoJSON().addTo(map);
+$.getJSON("/public/geojson/WIMIF.geojson",function(data){
+    WIMIF.addData(data);
+});
+
+var WISIE = L.geoJSON().addTo(map);
+$.getJSON("/public/geojson/WISIE.geojson",function(data){
+    WISIE.addData(data);
 });
 
 
-//var marker = L.marker([50.06, 19.94], {icon: greenIcon}).addTo(map);
-//var marker = L.marker([50.06, 19.95], {icon: redIcon}).addTo(map);
-//var marker = L.marker([50.06, 19.93], {icon: orageIcon}).addTo(map);
-//var marker = L.marker([50.06, 19.96], {icon: leafletIcon1}).addTo(map);
+var WM = L.geoJSON().addTo(map);
+$.getJSON("/public/geojson/WM.geojson",function(data){
+    WM.addData(data);
+});
 
-var marker2 = L.marker([50.08, 19.94]).addTo(map);
+var baseLayers = {
+};
+
+var overlays = {
+    "Jednostki pozawydzialowe": jednostkiPozawydzialowe,
+    "bydynki": bydynki,
+    "WA": WA,
+    "WIEIK": WIEIK,
+    "WIIT": WIIT,
+    "WIITCH": WIITCH,
+    "WIL":WIL,
+    "WIMIF":WIMIF,
+    "WISIE":WISIE,
+    "WM":WM
+};
+
+L.control.layers(baseLayers, overlays).addTo(map);
 
 
-var circle = L.circle([50.02, 19.94], {
-    color: ' red',
-    fillColor: '#ff0033',
-    fillOpacity: 0.5,
-    radius: 500
+
+WA.bindPopup(function (layer) {
+    return layer.feature.properties.description;
 }).addTo(map);
 
-var polygon = L.polygon([
-    [50.08, 19.97],
-    [50.09, 19.99],
-    [50.10, 19.98]
-]).addTo(map);
-
-
-L.control.scale({
-    metric: true,
-    imperial: false,
-    posiotion: 'bottomleft'
-}).addTo(map);
 
 
 
 
-//marker2.bindPopup("<b> Hey There!</b><br>Marker here").openPopup();
+
+
+
--- public/views/map.php
+++ public/views/map.php
@@ -5,8 +5,13 @@
     <title>Title</title>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+
+    <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+
     <link rel="stylesheet" type="text/css" href="public/css/map.css">
     <script type="text/javascript" src="./public/js/mapa.js" defer></script>
+
+
 
 </head>
 <body>
